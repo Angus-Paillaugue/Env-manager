6df0eb089b44a4eb73ce5c2cc47a125b5d11a2d9
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-03-27 11:31:35 +0100

    Home page WIP

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Hero from '$lib/components/home/hero/hero.svelte';
 	import { pageHeading } from '$lib/stores';
-	import { SECTION_HEIGHT } from '$lib/components/home/utils';
 	import Workflow from '$lib/components/home/workflow/workflow.svelte';
 
 	$pageHeading = {
@@ -12,7 +11,7 @@
 
 <Hero />
 
-<section class="px-2 py-12 lg:px-10 lg:py-24" style="height: {SECTION_HEIGHT};">
+<!-- <section class="px-2 py-12 lg:px-10 lg:py-24" style="height: {SECTION_HEIGHT};">
 	<div class="mx-auto grid h-full max-w-screen-2xl gap-4 lg:grid-cols-3 lg:grid-rows-2 lg:gap-10">
 		<div
 			class="bg-card border-border flex flex-col justify-between gap-2 rounded-lg border lg:col-span-1"
@@ -45,6 +44,6 @@
 		</div>
 		<div class="border-border rounded-xl border-2 border-dashed p-4 lg:col-span-1">Item 4</div>
 	</div>
-</section>
+</section> -->
 
 <Workflow />
