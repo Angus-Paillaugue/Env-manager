c9ed55fe673cd1b06ec85e3c39ec6f011a9943b9
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-03-27 11:37:33 +0100

    1.0.0 Release

---
+++ scripts/build-cli.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+thisDir=$(dirname $0)
+cliEntrypointLocation="${thisDir}/../cli/cli.ts"
+output="${thisDir}/../dist/env-manager"
+bun build "$cliEntrypointLocation" --compile --outfile "$output"
